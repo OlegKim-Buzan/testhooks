a469c310069d2d65773757f6a5c362d38bb13c16
Oleg <doomishe@mail.ru>
2022-01-28 18:57:32 +0300

    Signed-off-by: Oleg <doomishe@mail.ru>

--- hooks/pre-commit.py
+++ hooks/pre-commit.py
@@ -2,7 +2,6 @@ import re
 
 branch = 'dev'
 
-
 result_file = open("python\.bindings.txt", "w")
 source_file = open("python\.bindings.queue.txt", "r")
 standart_file = open("python\.bindings.standart.txt", "r")
@@ -16,25 +15,22 @@ connection = connection_file.read()
 
 for line in lines:
     if "#" in line:
-        if re.fullmatch('dev', line) != 'None' and branch == 'dev' :
+        if re.search('dev', line) is not None and branch == 'dev' :
             h = line.strip()
             l = h.find(' #')
             pieceofline = h[:l]
             newresult = standart
-            print(newresult)
             newresult = newresult.replace("@tagName@", pieceofline)
             result += newresult
-        if re.fullmatch('test', line) != 'None' and branch == 'test' :
+        if re.search(r'.*test.*', line) is not None and branch == 'test' :
             h = line.strip()
             l = h.find(' #')
             pieceofline = h[:l]
             newresult = standart
-            print(newresult)
             newresult = newresult.replace("@tagName@", pieceofline)
             result += newresult
     else:
         newresult = standart
-        print(newresult)
         newresult = newresult.replace("@tagName@", line.strip())
         result += newresult
         
