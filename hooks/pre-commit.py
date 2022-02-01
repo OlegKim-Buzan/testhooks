7d72dc0191cf44a2bb0d05232f8b9f177018648d
Oleg <doomishe@mail.ru>
2022-02-01 11:00:28 +0300

    Signed-off-by: Oleg <doomishe@mail.ru>

--- hooks/pre-commit.py
+++ hooks/pre-commit.py
@@ -11,18 +11,53 @@ connection_file = open("python\.bindings.connection.txt", "r")
 result = ''
 standart = standart_file.read()
 lines = source_file.readlines()
-connection = connection_file.read()
+#connection = connection_file.read()
 
 for line in lines:
     if "#" in line:
-        if re.search('dev', line) is not None and branch == 'dev' :
+        if re.search(r'.*#.*dev.*' , line) is not None and branch == 'dev' :
             h = line.strip()
             l = h.find(' #')
             pieceofline = h[:l]
             newresult = standart
             newresult = newresult.replace("@tagName@", pieceofline)
             result += newresult
-        if re.search('test', line) is not None and branch == 'test' :
+        if (re.search(r'.*#.*test\n.*', line) is not None or re.search(r'.*#.*test,.*', line) is not None) and branch == 'test' :
+            h = line.strip()
+            l = h.find(' #')
+            pieceofline = h[:l]
+            newresult = standart
+            newresult = newresult.replace("@tagName@", pieceofline)
+            result += newresult
+        if (re.search(r'.*#.*prod\n', line) is not None or re.search(r'.*#.*prod,', line) is not None) and branch == 'prod' :
+            h = line.strip()
+            l = h.find(' #')
+            pieceofline = h[:l]
+            newresult = standart
+            newresult = newresult.replace("@tagName@", pieceofline)
+            result += newresult
+        if re.search(r'.*#.*qf.*', line) is not None and branch == 'qf' :
+            h = line.strip()
+            l = h.find(' #')
+            pieceofline = h[:l]
+            newresult = standart
+            newresult = newresult.replace("@tagName@", pieceofline)
+            result += newresult
+        if re.search(r'.*#.*prodlike2.*', line) is not None and branch == 'prodlike2' :
+            h = line.strip()
+            l = h.find(' #')
+            pieceofline = h[:l]
+            newresult = standart
+            newresult = newresult.replace("@tagName@", pieceofline)
+            result += newresult
+        if re.search(r'.*#.*test2.*', line) is not None and branch == 'test2' :
+            h = line.strip()
+            l = h.find(' #')
+            pieceofline = h[:l]
+            newresult = standart
+            newresult = newresult.replace("@tagName@", pieceofline)
+            result += newresult
+        if (re.search(r'.*#.*prodlike\n.*', line) is not None or re.search(r'.*#.*prodlike,.*', line) is not None) and branch == 'prodlike' :
             h = line.strip()
             l = h.find(' #')
             pieceofline = h[:l]
@@ -33,8 +68,16 @@ for line in lines:
         newresult = standart
         newresult = newresult.replace("@tagName@", line.strip())
         result += newresult
-        
-result += connection
+
+curLineBranch = ''
+conlines = connection_file.readlines()
+for conline in conlines: 
+    if "#" in line:
+        curLineBranch = conline
+    else:
+        if branch in curLineBranch or curLineBranch == '':
+            result += conline
+
 result_file.write(result)
 
 result_file.close()
